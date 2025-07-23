493a2fa5c788316c6af4fb4d36504cf2bbc2fbf6
rafaiska <paocomcarnemoidaetubaina@gmail.com>
2025-07-23 13:45:34 -0300

    first

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -45,6 +45,7 @@
 #include "tests/core/input/test_input_event_mouse.h"
 #include "tests/core/input/test_shortcut.h"
 #include "tests/core/io/test_config_file.h"
+#include "tests/core/io/test_e_net_connection.h"
 #include "tests/core/io/test_file_access.h"
 #include "tests/core/io/test_http_client.h"
 #include "tests/core/io/test_image.h"
